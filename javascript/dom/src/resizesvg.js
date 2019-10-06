4b192affbffeed83182349921e3d3d710bee3ff0
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-06 13:16:25 -0300

    trying to do the 3 new growSquares

--- javascript/dom/src/resizesvg.js
+++ javascript/dom/src/resizesvg.js
@@ -12,23 +12,46 @@ class Movel{
       this.up = this.up.bind(this);
       this.grow = this.grow.bind(this);
       this.notGrow = this.notGrow.bind(this);
+
+
       this.position = { "dx" : 0,
                         "dy": 0,
                         "tx":0,
                         "ty":0};
       this.follow = false;
       this.resize = false;
-      this.growSquare = document.querySelector(".pointerDiff");
-      this.growSquare.classList.toggle("visible");
+
+      this.controlDown = false;
+
+      this.growSquareBR = document.querySelector("#squareBR");
+      this.growSquareBR.classList.toggle("visible");
+
+      this.growSquareTL = document.querySelector("#squareTL");
+      this.growSquareTL.classList.toggle("visible");
+      this.growSquareBL = document.querySelector("#squareBL");
+      this.growSquareBL.classList.toggle("visible");
+      this.growSquareTR = document.querySelector("#squareTR");
+      this.growSquareTR.classList.toggle("visible");
+
       this.square = document.querySelector("#move");
       this.area = document.querySelector("#area");
-      this.growSquare.addEventListener("mousedown", this.grow);
-      this.growSquare.addEventListener("mouseup", this.notGrow);
+      this.growSquareBR.addEventListener("mousedown", this.grow);
+      this.growSquareBR.addEventListener("mouseup", this.notGrow);
+
+      this.growSquareTL.addEventListener("mousedown", this.grow);
+      this.growSquareTL.addEventListener("mouseup", this.notGrow);
+      this.growSquareBL.addEventListener("mousedown", this.grow);
+      this.growSquareBL.addEventListener("mouseup", this.notGrow);
+      this.growSquareTR.addEventListener("mousedown", this.grow);
+      this.growSquareTR.addEventListener("mouseup", this.notGrow);
+
       this.square.addEventListener("mousedown", this.down);
       this.square.addEventListener("mouseup",this.up);
       this.area.addEventListener("mousemove", this.move);
       this.group = document.querySelector("#group-move");
       // this.area.addEventListener("click", this.click);
+
+      document.addEventListener("keydown", this.move);
 
    }
    grow(event){
@@ -36,13 +59,28 @@ class Movel{
    }
    down(event) {
       this.follow = true;
-      this.growSquare.classList.toggle("visible");
+      this.growSquareBR.classList.toggle("visible");
+
+      this.growSquareTL.classList.toggle("visible");
+      this.growSquareBL.classList.toggle("visible");
+      this.growSquareTR.classList.toggle("visible");
+
       this.position.dx = event.x - this.position.tx;
       this.position.dy = event.y - this.position.ty;
       // this.position.dx = event.x - Number(this.group.getAttribute("transform"));
       // this.position.dy = event.y - Number(this.group.getAttribute("y"));
    }
    move(event) {
+    if(event.ctrlKey){
+        this.square.setAttribute("preserveAspectRatio", "xMidYMid");
+        this.controlDown = true;
+        
+    }
+    else{
+        this.square.setAttribute("preserveAspectRatio", "none");
+        this.controlDown = false;
+    }
+
       if (this.follow && !this.resize) {
          if(event.x < 0){
             event.x = 0;
@@ -53,13 +91,26 @@ class Movel{
          this.position.tx = event.x - this.position.dx;
          this.position.ty = event.y - this.position.dy;
          this.group.setAttribute("transform","translate(" + (this.position.tx) + "," + (this.position.ty) + ")");
-         // this.group.setAttribute("y",event.y - this.position.dy);
       }
       if (this.resize){
-         this.square.setAttribute("width",event.x - this.position.tx);
-         this.square.setAttribute("height",event.y - this.position.ty);
-         this.growSquare.setAttribute("x", event.x - this.position.tx -5 );
-         this.growSquare.setAttribute("y", event.y - this.position.ty -5);
+         let squareSizeX = event.x - this.position.tx;
+         let squareSizeY = event.y - this.position.ty;
+         
+         if(squareSizeX - 5 >=0 && squareSizeY-5 >= 0){
+            if(this.controlDown===false){
+                this.square.setAttribute("width",squareSizeX);
+                this.square.setAttribute("height",squareSizeY);
+                this.growSquareBR.setAttribute("x", squareSizeX -5 );
+                this.growSquareBR.setAttribute("y", squareSizeY -5);
+            }
+            else{
+                let minimun = Math.min(squareSizeX, squareSizeY);
+                this.square.setAttribute("width",minimun);
+                this.square.setAttribute("height",minimun);
+                this.growSquareBR.setAttribute("x", minimun -5 );
+                this.growSquareBR.setAttribute("y", minimun -5);
+            }
+         }
       }
    }
    up(event) {
@@ -67,10 +118,6 @@ class Movel{
    }
    notGrow(event){
       this.resize = false;
-      this.growSquare.classList.toggle("visible");
+      this.growSquareBR.classList.toggle("visible");
    }
 }
-
-//(function() {
-   //Movel.follow = false;
-//})();
