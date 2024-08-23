3ed23442e0a9c527a78976f76bb62d17901e5e7e
André Santanchè <santanche@gmail.com>
2024-08-23 13:00:07 -0300

    feat (frameworks/react): external js file

--- frameworks/react/2-js/src/script.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <button>Hello React!</button>
-)
---
+++ frameworks/react/3-js-file/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+)
+
+root.render(
+  <h1>Velocirest</h1>
+)
