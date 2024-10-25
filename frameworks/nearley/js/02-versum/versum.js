e1427d125013a1e8728d0bf18a95e5cef117ce40
André Santanchè <santanche@gmail.com>
2024-10-25 09:43:44 -0300

    feat (frameworks/compiler): Nearley and Antlr

--- frameworks/nearley/js/02-versum/versum.js
+++ frameworks/nearley/js/02-versum/versum.js
@@ -1,8 +1,8 @@
 // Generated automatically by nearley, version 2.20.1
 // http://github.com/Hardmath123/nearley
-(function () {
+(() => {
 function id(x) { return x[0]; }
-var grammar = {
+const grammar = {
     Lexer: undefined,
     ParserRules: [
     {"name": "_$ebnf$1", "symbols": []},
