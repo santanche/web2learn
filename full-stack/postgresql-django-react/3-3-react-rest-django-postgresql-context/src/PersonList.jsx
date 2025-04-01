1776af00708c9cbabbbbd94daa05d4d4ed40c848
André Santanchè <santanche@gmail.com>
2025-04-01 11:26:23 -0300

    refactor (full-stack): split from two React solutions

--- full-stack/postgresql-django-react/3-3-react-rest-django-postgresql-context/src/PersonList.jsx
+++ full-stack/postgresql-django-react/3-3-react-rest-django-postgresql-context/src/PersonList.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
+const PersonContext = React.createContext({
+  personList: [], fetchPersonList: () => {}
+})
+
 function PersonList() {
   const [personList, setPersonList] = useState([])
 
@@ -19,13 +23,15 @@ function PersonList() {
         <tr><th>Name</th><th>E-mail</th><th>Birthday</th></tr>
       </thead>
       <tbody>
-        {personList.map(person => (
-          <tr key={person.email_id}>
-            <td><b>{person.name}</b></td>
-            <td>{person.email_id}</td>
-            <td>{person.birthday}</td>
-          </tr>
-        ))}
+        <PersonContext.Provider value={{personList, fetchPersonList}}>
+          {personList.map(person => (
+            <tr key={person.email_id}>
+              <td><b>{person.name}</b></td>
+              <td>{person.email_id}</td>
+              <td>{person.birthday}</td>
+            </tr>
+          ))}
+        </PersonContext.Provider>
       </tbody>
     </table>
   )
